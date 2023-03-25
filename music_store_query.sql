d0cf5315f4afc6fcadc3481f94cba32c2c74c314
Ashutosh Rajput <104626623+AshutosRajput@users.noreply.github.com>
2023-03-25 20:53:37 +0530

    Update music_store_query.sql

--- music_store_query.sql
+++ music_store_query.sql
@@ -13,8 +13,8 @@ order by count desc
 select total from invoice 
 order by total desc
 
----which city has the best customers we would like to throw a promotional music festival in city
----we made the most money rite a query that return one city that has the highest sum of invoices total returns both the city thatv has yhe highest sum of invoices total
+---which city has the best customers we would like to throw a promotional music festival in city we made the most money rite a query 
+that return one city that has the highest sum of invoices total returns both the city thatv has yhe highest sum of invoices total
 select sum(total)as invoice_total,billing_city from invoice
 group by billing_city
 order by invoice_total desc
@@ -27,8 +27,7 @@ group by customer.customer_id
 order by total desc
 limit 1
 
----write a query to return the email,first name,last name & genre of all rock
-music lisner .return your list orderd alphabetically by email starting with A
+---write a query to return the email,first name,last name & genre of all rock music lisner .return your list orderd alphabetically by email starting with A
 
 select distinct email,first_name,last_name
 from customer
@@ -41,9 +40,8 @@ where track_id in(
 )
 order by email;
 
-question 8
-lets invite the artist who have written the most rock music in our data set.write a query 
-that returns the artist name and total track count of the top 10 rock brand
+
+---lets invite the artist who have written the most rock music in our data set.write a query that returns the artist name and total track count of the top 10 rock brand
 select artist.artist_id,artist.name,count(artist.artist_id) as number_of_songs
 from track
 join album on album.album_id=track.album_id
@@ -54,8 +52,7 @@ group by artist.artist_id
 order by number_of_songs desc
 limit 10;
 
-return all the track names that have a song length longer than the average song lenght  return the name
-and milisecond for each track order by the song lenght withe the longest song listed first
+---return all the track names that have a song length longer than the average song lenght  return the name and milisecond for each track order by the song lenght withe the longest song listed first
 
 select name,milliseconds from track
 where milliseconds >(
@@ -64,7 +61,7 @@ where milliseconds >(
 )
 order by milliseconds desc
 
-find how much amount spent by each customer on artist write a query to return customer name and total artist
+---find how much amount spent by each customer on artist write a query to return customer name and total artist
 
 with best_selling_artist as(
 select artist.artist_id as artist_id,artist.name as artist_name,sum(invoice_line.unit_price*invoice_line.quantity)as total_sales
@@ -87,7 +84,7 @@ join best_selling_artist bsa on bsa.artist_id=alb.artist_id
 group by 1,2,3,4
 order by  5 desc;
 
-8-we want to find out the most popular music genre for each country.we determine
+---we want to find out the most popular music genre for each country.we determine
 the most popular genre as the genre with the highest amount of purchase write a query that return
 each country along with the top genre for countries where the maximum number of purchase is shared
  return all genres
